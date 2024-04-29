93b0274d7b0e98ccd1739107d5ccd31c800e4b95
yuri.dunets <yuri.dunets@hiqo-solutions.com>
2024-04-29 23:02:49 +0400

    feat: added custom deploy action to S3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,24 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
+// const github = require('@actions/github');
 
 function run() {
+  // 1) Get the input values
+  const bucket = core.getInput('bucket', {
+    required: true
+  });
+  const bucketRegion = core.getInput('bucket-region', {
+    required: true
+  });
+  const distFolder = core.getInput('dist-folder', {
+    required: true
+  });
+
+  const s3Uri= `s3://${bucket}`;
+
+  // 2) Upload files to S3
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
   core.notice('Hello from my custom JavaScript action!');
 }
 
